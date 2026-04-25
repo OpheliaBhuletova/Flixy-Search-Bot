71d2bcbc5c03901b431dc4f7f6a32b872426e0ea
OpheliaBhuletova <mihidtracs@yahoo.com>
2026-04-25 11:17:51 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -49,7 +49,13 @@ async def inline_query_handler(client: Client, query: InlineQuery):
         file_type = None
 
     reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton(" Search again", switch_inline_query_current_chat=keyword)]]
+        [[
+            InlineKeyboardButton(
+                text=" Search again", 
+                switch_inline_query_current_chat=keyword,
+                style="primary"  # This makes the button Blue
+            )
+        ]]
     )
 
     files, next_offset, total = await get_search_results(
